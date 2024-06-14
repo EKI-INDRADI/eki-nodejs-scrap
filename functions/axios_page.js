3c55e4c251b9dbe046bbacf303b8a5cb3e7a802f
EKI-INDRADI <eki.indradi@gmail.com>
2024-06-15 00:18:01 +0700

    FIX ERROR 403

--- functions/axios_page.js
+++ functions/axios_page.js
@@ -5,10 +5,33 @@ let browser_useragent = require('./browser_useragent')
 exports.axiosGetWithRetry = async function (url, retries = 3, id = null) {
     for (let attempt = 1; attempt <= retries; attempt++) {
         try {
-            
-            return await axios.get(url, { headers : browser_useragent.setHeaders() })
+
+            return await axios.get(url, {
+                headers: {
+                    ...browser_useragent.setHeaders(),
+                    'authority': `${String(url).split('/')[2]}`,
+                    'method': `GET`,
+                    'path': String(url).replace(`${String(url).split('/')[0]}/${String(url).split('/')[1]}/${String(url).split('/')[2]}`, ``),
+                    'scheme': `https`,
+                    Accept: `text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7`,
+                    'Accept-Encoding': `gzip, deflate, br, zstd`,
+                    'Accept-Language': `en-US,en;q=0.9,id;q=0.8`,
+                    'Cookie': `${Date.now()}/${Math.random()}.${Math.random()}.${Math.random()}.${Math.random()}`,
+                    'Priority':`u=0, i`,
+                    'Sec-Ch-Ua' : `"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"`,
+                    'Sec-Ch-Ua-Mobile' : `?0`,
+                    'Sec-Ch-Ua-Platform': `"Windows"`,
+                    'Sec-Fetch-Dest' : `document`,
+                    'Sec-Fetch-Mode' :`navigate`,
+                    'Sec-Fetch-Site' : `same-origin`,
+                    'Sec-Fetch-User' : `?1`,
+                    'Upgrade-Insecure-Requests' : 1,
+                    Referer: `${String(url).split('/')[0]}/${String(url).split('/')[1]}/${String(url).split('/')[2]}`
+                }
+            })
 
         } catch (error) {
+    
             // console.error(`${id} | GET PAGE HTML | RE-TRY ${attempt} | ${url} | ERROR : ${error}`);
             //  console.error(`${id} | GET PAGE HTML | RE-TRY ${attempt} | ERROR : ${error}`);
             if (error.response.status >= 403 || error?.status) {
--- functions/download_file.js
+++ functions/download_file.js
@@ -4,33 +4,53 @@ const fs = require('fs');
 let browser_useragent = require('./browser_useragent')
 // Fungsi untuk mengunduh file dengan retry manual
 exports.downloadFileWithRetry = async function (url, outputLocationPath, retries = 3, id = null) {
-    const writer = fs.createWriteStream(outputLocationPath);
-  
-    for (let attempt = 1; attempt <= retries; attempt++) {
-  
-      try {
-        const response = await axios({
-          url,
-          method: 'GET',
-          responseType: 'stream',
-          headers : browser_useragent.setHeaders()
-        });
-  
-        response.data.pipe(writer);
-  
-        return new Promise((resolve, reject) => {
-          writer.on('finish', resolve);
-          writer.on('error', reject);
-        });
-      } catch (error) {
-        error_detail.try_catch_error_detail(error)
-        // console.error(`${id} | GET DOWNLOAD FILE | RE-TRY ${attempt} | ${url} | ERROR : ${error}`);
-        console.error(`${id} | GET DOWNLOAD FILE | RE-TRY ${attempt} | ERROR : ${error}`);
-        if (attempt === retries) {
-          throw error;
+  const writer = fs.createWriteStream(outputLocationPath);
+
+  for (let attempt = 1; attempt <= retries; attempt++) {
+
+    try {
+      const response = await axios({
+        url,
+        method: 'GET',
+        responseType: 'stream',
+        headers: {
+          ...browser_useragent.setHeaders(),
+          'authority': `${String(url).split('/')[2]}`,
+          'method': `GET`,
+          'path': String(url).replace(`${String(url).split('/')[0]}/${String(url).split('/')[1]}/${String(url).split('/')[2]}`, ``),
+          'scheme': `https`,
+          Accept: `text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7`,
+          'Accept-Encoding': `gzip, deflate, br, zstd`,
+          'Accept-Language': `en-US,en;q=0.9,id;q=0.8`,
+          'Cookie': `${Date.now()}/${Math.random()}.${Math.random()}.${Math.random()}.${Math.random()}`,
+          'Priority': `u=0, i`,
+          'Sec-Ch-Ua': `"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"`,
+          'Sec-Ch-Ua-Mobile': `?0`,
+          'Sec-Ch-Ua-Platform': `"Windows"`,
+          'Sec-Fetch-Dest': `document`,
+          'Sec-Fetch-Mode': `navigate`,
+          'Sec-Fetch-Site': `same-origin`,
+          'Sec-Fetch-User': `?1`,
+          'Upgrade-Insecure-Requests': 1,
+          Referer: `${String(url).split('/')[0]}/${String(url).split('/')[1]}/${String(url).split('/')[2]}`
         }
-        // Tunggu sebelum mencoba lagi
-        await new Promise(resolve => setTimeout(resolve, attempt * 2000));
+      });
+
+      response.data.pipe(writer);
+
+      return new Promise((resolve, reject) => {
+        writer.on('finish', resolve);
+        writer.on('error', reject);
+      });
+    } catch (error) {
+      error_detail.try_catch_error_detail(error)
+      // console.error(`${id} | GET DOWNLOAD FILE | RE-TRY ${attempt} | ${url} | ERROR : ${error}`);
+      console.error(`${id} | GET DOWNLOAD FILE | RE-TRY ${attempt} | ERROR : ${error}`);
+      if (attempt === retries) {
+        throw error;
       }
+      // Tunggu sebelum mencoba lagi
+      await new Promise(resolve => setTimeout(resolve, attempt * 2000));
     }
   }
+}
